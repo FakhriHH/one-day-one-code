ca7e793e675fc6d78898c2246d02778de2438302
FakhriHH <fakhrihabib9@gmail.com>
2025-01-05 13:44:16 +0700

    Day 19: Pembalik Array

---
+++ day-19/pembalik-array.js
@@ -0,0 +1,11 @@
+function reverseArray(array) {
+    return array.reverse();
+}
+
+const originalArray = [1, 2, 3, 4, 5];
+
+const reversedArray = reverseArray(originalArray);
+  
+console.log("Array asli:", originalArray);
+console.log("Array setelah dibalik:", reversedArray);
+  
